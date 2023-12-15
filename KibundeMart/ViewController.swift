5d3a736987b166f3ca4cca75f1302a856e5c2bc8
Kitune <daisuke3429@icloud.com>
2023-12-15 19:05:09 +0900

    Initial Commit

---
+++ KibundeMart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KibundeMart
+//
+//  Created by 秋武大介 on 2023/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
