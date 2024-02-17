e716241644d190784b44305f64d32cce2ed73177
Kitune <daisuke3429@icloud.com>
2024-02-17 14:35:04 +0900

    ジャンル分けての表示ができない！

--- KibundeMart/CollectionView.swift
+++ KibundeMart/CollectionView.swift
@@ -18,7 +18,9 @@ class CollectionView: UIViewController,UICollectionViewDelegate,
     
     var outputValue : String?
     
-    @IBOutlet var collectionView: UICollectionView!
+    var filteredItems: [[String: Any]] = []
+    
+   @IBOutlet var collectionView: UICollectionView!
     
     let spacer: CGFloat = 16
     
@@ -30,27 +32,33 @@ class CollectionView: UIViewController,UICollectionViewDelegate,
         
         let layout = UICollectionViewFlowLayout()
         // 縦スクロールに設定
-
+        
         layout.scrollDirection = .vertical
         // セルの大きさ設定
-
+        
         layout.itemSize = CGSize(width: view.frame.width / 2 - spacer * 2, height:
-        view.frame.width / 2 - spacer * 2)
+                                    view.frame.width / 2 - spacer * 2)
         // 余白の設定
-
+        
         layout.sectionInset = UIEdgeInsets(top: spacer, left: spacer, bottom: spacer, right: spacer)
         // レイアウトをcollectionViewに適応させる
-
+        
         collectionView.collectionViewLayout = layout
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         itemList = saveData.object(forKey: "item_data") as! [[String : Any]]
         collectionView.reloadData()
+        
+        let filteredItems = itemList.filter { item in
+            guard let feeling = item["feeling"] as? String else { return false }
+            return feeling == outputValue
+        }
     }
     //コレクションビューで配列の分セルを表示する！
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-       return itemList.count
+        return itemList.count
     }
     //コレクションビューのセルの中に何を表示するか！
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -60,7 +68,7 @@ class CollectionView: UIViewController,UICollectionViewDelegate,
         
         let imageView = cell.contentView.viewWithTag(1) as! UIImageView
         imageView.image = UIImage(data: itemList[indexPath.row]["itemImage"] as! Data)
-
+        
         let titleLabel = cell.contentView.viewWithTag(2) as! UILabel
         titleLabel.text = itemList[indexPath.row]["itemName"] as? String
         
@@ -69,7 +77,9 @@ class CollectionView: UIViewController,UICollectionViewDelegate,
     //セルをタップした際に何をするか！
     func collectionView(_ collectionView: UICollectionView,
                         didSelectItemAt indexPath: IndexPath) {
-    // Identifierを指定して画面遷移する
-    performSegue(withIdentifier: "toSecondVC", sender: nil)
+        // Identifierを指定して画面遷移する
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
     }
+    
 }
+
