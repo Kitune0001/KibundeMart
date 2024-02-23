fa1836244b8233af0b1bb07cac03dd8a94ff5a42
Kitune <daisuke3429@icloud.com>
2024-02-23 20:05:45 +0900

    Searchボタンでフィルターかけず全部を表示できるようにした！

--- KibundeMart/CollectionView.swift
+++ KibundeMart/CollectionView.swift
@@ -30,34 +30,36 @@ class CollectionView: UIViewController,UICollectionViewDelegate,
         
         let layout = UICollectionViewFlowLayout()
         // 縦スクロールに設定
-
+        
         layout.scrollDirection = .vertical
         // セルの大きさ設定
-
+        
         layout.itemSize = CGSize(width: view.frame.width / 2 - spacer * 2, height:
-        view.frame.width / 2 - spacer * 2)
+                                    view.frame.width / 2 - spacer * 2)
         // 余白の設定
-
+        
         layout.sectionInset = UIEdgeInsets(top: spacer, left: spacer, bottom: spacer, right: spacer)
         // レイアウトをcollectionViewに適応させる
-
+        
         collectionView.collectionViewLayout = layout
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-                itemList = saveData.object(forKey: "item_data") as! [[String : Any]]
-                let filteredItems = itemList.filter { item in
-                    guard let feeling = item["feeling"] as? String else { return false }
-                    return feeling == outputValue!
-                }
-                
-                itemList = filteredItems
-                collectionView.reloadData()
+        itemList = saveData.object(forKey: "item_data") as! [[String : Any]]
+        if (outputValue != "Search"){
+            let filteredItems = itemList.filter { item in
+                guard let feeling = item["feeling"] as? String else { return false }
+                return feeling == outputValue!
+            }
             
+            itemList = filteredItems
+        }
+        collectionView.reloadData()
+        
     }
     //コレクションビューで配列の分セルを表示する！
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-       return itemList.count
+        return itemList.count
     }
     //コレクションビューのセルの中に何を表示するか！
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -67,7 +69,7 @@ class CollectionView: UIViewController,UICollectionViewDelegate,
         
         let imageView = cell.contentView.viewWithTag(1) as! UIImageView
         imageView.image = UIImage(data: itemList[indexPath.row]["itemImage"] as! Data)
-
+        
         let titleLabel = cell.contentView.viewWithTag(2) as! UILabel
         titleLabel.text = itemList[indexPath.row]["itemName"] as? String
         
@@ -76,8 +78,8 @@ class CollectionView: UIViewController,UICollectionViewDelegate,
     //セルをタップした際に何をするか！
     func collectionView(_ collectionView: UICollectionView,
                         didSelectItemAt indexPath: IndexPath) {
-    // Identifierを指定して画面遷移する
-    performSegue(withIdentifier: "toSecondVC", sender: nil)
+        // Identifierを指定して画面遷移する
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- KibundeMart/SelectViewController.swift
+++ KibundeMart/SelectViewController.swift
@@ -22,52 +22,59 @@ class SelectViewController: UIViewController {
     
     @IBOutlet weak var inputField: UITextField!
     
+    @IBOutlet weak var search: UIButton!
+    
     var feeling: String?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     // segueが動作することをViewControllerに通知するメソッド
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-            // segueのIDを確認して特定のsegueのときのみ動作させる
-            if segue.identifier == "toBViewController" {
-                //  遷移先のViewControllerを取得
-                let next = segue.destination as? CollectionView
-                // 用意した遷移先の変数に値を渡す
-                next?.outputValue = feeling
-            }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // segueのIDを確認して特定のsegueのときのみ動作させる
+        if segue.identifier == "toBViewController" {
+            //  遷移先のViewControllerを取得
+            let next = segue.destination as? CollectionView
+            // 用意した遷移先の変数に値を渡す
+            next?.outputValue = feeling
         }
-
-        @IBAction func tapTransitionButton(_ sender: Any) {
-            // 画面遷移実行
-            performSegue(withIdentifier: "toBViewController", sender: nil)
-        }
-
+    }
+    
+    @IBAction func tapTransitionButton(_ sender: Any) {
+        // 画面遷移実行
+        performSegue(withIdentifier: "toBViewController", sender: nil)
+    }
+    
     @IBAction func tapCreative(){
         //押されたら一時的に変数に値を格納する！
-           feeling = "Creative"
+        feeling = "Creative"
         // 画面遷移実行
-            performSegue(withIdentifier: "toBViewController", sender: nil)
+        performSegue(withIdentifier: "toBViewController", sender: nil)
     }
-
+    
     @IBAction func tapHappy(){
-           //押されたら一時的に変数に値を格納する！
-           feeling = "Happy"
+        //押されたら一時的に変数に値を格納する！
+        feeling = "Happy"
         // 画面遷移実行
-            performSegue(withIdentifier: "toBViewController", sender: nil)
-       }
+        performSegue(withIdentifier: "toBViewController", sender: nil)
+    }
     @IBAction func tapRelax(){
         //押されたら一時的に変数に値を格納する！
         feeling = "Relax"
         // 画面遷移実行
-            performSegue(withIdentifier: "toBViewController", sender: nil)
+        performSegue(withIdentifier: "toBViewController", sender: nil)
     }
     @IBAction func tapHarry(){
         //押されたら一時的に変数に値を格納する！
         feeling = "Harry"
         // 画面遷移実行
-            performSegue(withIdentifier: "toBViewController", sender: nil)
+        performSegue(withIdentifier: "toBViewController", sender: nil)
     }
-
+    @IBAction func tapSearch(){
+        //押されたら一時的に変数に値を格納する！
+        feeling = "Search"
+        // 画面遷移実行
+        performSegue(withIdentifier: "toBViewController", sender: nil)
+    }
 }
